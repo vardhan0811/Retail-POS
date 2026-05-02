7afa55f0d4de2162436f93646e49ff5c32842a50
Vardhan Rayapureddy <rayapureddyvardhan2004@gmail.com>
2026-05-02 15:33:58 +0530

    update all changes

--- runall.ps1
+++
@@ -1,5 +0,0 @@
-npx concurrently -n auth,product,order,gateway `
-"cd AuthService && dotnet run" `
-"cd ProductService && dotnet run" `
-"cd OrderService && dotnet run" `
-"cd ApiGateway && dotnet run"
